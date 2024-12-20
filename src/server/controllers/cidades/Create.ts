862ffed2e05d8894888fed10bde08601ef949eba
Samara <samarabsouz@gmail.com>
2024-12-19 22:38:31 -0300

    feat: add custom error messages for yup validation and update cidades creation logic

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 //import { StatusCodes } from "http-status-codes";
 import * as yup from 'yup';
 
 
 interface ICidade {
   nome: string;
+  estado: string;
 };
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(2),
 });
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
@@ -16,15 +19,17 @@ export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
 let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
-
-    res.json({
-      errors: {
-        default: yupError.message,
-      }
+    validatedData = await bodyValidation.validate(req.body, {abortEarly: false});
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+  
+    yupError.inner.forEach(error => {
+      if (error.path === undefined) return;
+      errors[error.path] = error.message;
     });
+
+    res.status(StatusCodes.BAD_REQUEST).json({ errors });
     return
   }
 
