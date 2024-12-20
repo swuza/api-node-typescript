a19acf37aa13c4fe5e9e3c7510b482a570a106e2
Samara <samarabsouz@gmail.com>
2024-12-19 22:14:52 -0300

    feat: add yup for request body validation in cidades creation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,14 +1,34 @@
 import { Request, Response } from "express";
-
+//import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 
 interface ICidade {
   nome: string;
 };
 
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
+
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-  console.log(req.body.nome);
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+let validatedData: ICidade | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    res.json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+    return
+  }
+
+  console.log(validatedData);
 
 
 
