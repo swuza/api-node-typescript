a155649ac56e7c2dc1bf35817c264b30f0906b5e
Samara <samarabsouz@gmail.com>
2024-12-28 18:15:36 -0300

    feat: add Knex configuration for SQLite and update .gitignore to include .sqlite

---
+++ src/server/database/knex/@types/knex.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+
+
+declare module 'knex/types/tables' {
+  interface Tables {
+    // cidade: Cidade;
+    // pessoa: Pessoa;
+    // usuario: Usuario;
+  }
+};
